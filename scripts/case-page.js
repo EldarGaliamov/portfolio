efdc751bca8761fda0aa0d2ee37b67a53cef9b5e
EldarGaliamov <eldargaliamov19@gmail.com>
2026-06-10 03:40:07 +0300

    резюме

--- scripts/case-page.js
+++ scripts/case-page.js
@@ -62,7 +62,7 @@
           <span>Eldar Galiamov</span>
         </a>
         <nav class="home-header__nav">
-          <a href="../index.html#resume">Резюме</a>
+          <a href="../assets/Files/Галямов Эльдар Резюме.pdf" target="_blank" rel="noopener noreferrer">Резюме</a>
           <a href="../index.html#cv">CV</a>
           <a class="ui-button ui-button--m ui-button--black" href="https://t.me/eldarglmv" target="_blank" rel="noreferrer">Telegram</a>
         </nav>
@@ -552,4 +552,3 @@
     });
   }
 })();
-
